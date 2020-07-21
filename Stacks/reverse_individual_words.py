2436ada3342b3ae2d6864bb166ac9d3d9e907a23
whopriyam <priyambasu16@gmail.com>
2020-07-22 00:18:15 +0530

    Reversing words in a string using stack

---
+++ Stacks/reverse_individual_words.py
@@ -0,0 +1,19 @@
+def reverserWords(string): 
+    string = string + " "
+    stack = [] 
+  
+    for i in range(len(string)): 
+        if string[i] != " ": 
+            stack.append(string[i]) 
+            
+        else: 
+            while len(stack) > 0: 
+                print(stack[-1], end= "") 
+                stack.pop() 
+            print(end = " ") 
+   
+  
+# Driver Code 
+if __name__ == "__main__": 
+    string = "Hello this is Skynet"
+    reverserWords(string) 
